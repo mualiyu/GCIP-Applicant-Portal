a9c5a8e3e920e6eefdf384becb6bbf3544003500
mubeess <mubarakibrahim2015@gmail.com>
2023-05-27 15:19:24 +0100

    made changes to the sms flow

--- src/components/MyModal.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './styles/modal.css'
-
-export default function MyModal({isOpen,children}) {
-  return (
-    <div className={`my-modal ${isOpen?'active':null}`}>{children}</div>
-  )
-}
--- src/helpers/convertDate.ts
+++
@@ -1,5 +0,0 @@
-export default function convertDate(date:string) {
-const converted=date.split('T')[0]
-
-return converted
-}
